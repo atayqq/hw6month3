086af549726ef1cf057cdbc24b0b7dd571fc9e90
atayqq <ataykylychbekov96@gmail.com>
2023-04-28 19:47:48 +0600

    Initial Commit

---
+++ hww6month3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hww6month3
+//
+//  Created by atay on 28/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
